5ccef2bcb9e5a25a9a874cf0101cdee38e31d6e3
Vijay Krishna Narsingoju <70138450+vknan@users.noreply.github.com>
2024-04-28 16:43:12 +0530

    cors allowed

--- Vijay_Portfolio/myproject/settings/base.py
+++ Vijay_Portfolio/myproject/settings/base.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 ]
 SITE_ID = 1
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',  # Example: React frontend URL
+    'https://react-django-full-stack.vercel.app/',  # Example: React frontend URL
     # Add more origins as needed
 ]
 
