5673ccfa68134ec4cdc3f1b9cb838d8887da47c1
Vijay Krishna Narsingoju <70138450+vknan@users.noreply.github.com>
2024-04-28 15:59:22 +0530

    WSGI Production Update

--- Vijay_Portfolio/myproject/wsgi.py
+++ Vijay_Portfolio/myproject/wsgi.py
@@ -7,10 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
+
 import os
+import sys
 
+# Add your project directory to the sys.path
+project_home = '/home/vknan/NAVIKONLKINE/React_Django_Full_Stack/Vijay_Portfolio/'  # Path to your Django project directory
+if project_home not in sys.path:
+    sys.path.append(project_home)
+
+# Set environment variable to use your project's settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings.production")
+
+# Configure Django application
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings.development")
-
 application = get_wsgi_application()
