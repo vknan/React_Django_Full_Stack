551710913739e16f408081cd86ca2446c49e2609
Vijay Krishna Narsingoju <70138450+vknan@users.noreply.github.com>
2024-04-30 21:50:56 +0530

    small update

--- Vijay_Portfolio/myproject/settings/base.py
+++ Vijay_Portfolio/myproject/settings/base.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 CORS_ALLOWED_ORIGINS = [
     'https://react-django-full-stack.vercel.app/',
-    'https://navikonline.in'  # Example: React frontend URL
+    'https://navikonline.in',  # Example: React frontend URL
     # Add more origins as needed
 ]
 
