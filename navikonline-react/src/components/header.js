50afe71299187e471cc6ddeba63f84efd13910d5
Vijay Krishna Narsingoju <70138450+vknan@users.noreply.github.com>
2024-05-01 16:24:10 +0530

    Hamburger menu update

--- navikonline-react/src/components/header.js
+++ navikonline-react/src/components/header.js
@@ -1,13 +1,19 @@
-import React from "react";
-
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import "./header.css";
 
 const Header = (props) => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const handleToggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <header data-role="Header" className="header-header">
+    <header className="header-header">
       <img alt={props.imageAlt} src={props.imageSrc} className="header-image" />
+
       <div className="header-nav">
         <nav className="header-nav1">
           <span className="header-text">
@@ -21,42 +27,48 @@ const Header = (props) => {
           <span className="header-text4">{props.text4}</span>
         </nav>
       </div>
+
       <div className="header-btn-group">
         <button className="header-login button">{props.login}</button>
         <button className="button">{props.register}</button>
       </div>
-      <div data-role="BurgerMenu" className="header-burger-menu">
+
+      <div className="header-burger-menu" onClick={handleToggleMobileMenu}>
         <svg viewBox="0 0 1024 1024" className="header-icon">
           <path d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"></path>
         </svg>
       </div>
-      <div data-role="MobileMenu" className="header-mobile-menu">
-        <div className="header-nav2">
-          <div className="header-container">
-            <img
-              alt={props.imageAlt1}
-              src={props.imageSrc1}
-              className="header-image1"
-            />
-            <div data-role="CloseMobileMenu" className="header-menu-close">
-              <svg viewBox="0 0 1024 1024" className="header-icon02">
-                <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
-              </svg>
+
+      {isMobileMenuOpen && (
+        <div className="header-mobile-menu">
+          <div className="header-nav2">
+            <div className="header-container">
+              <img
+                alt={props.imageAlt1}
+                src={props.imageSrc1}
+                className="header-image1"
+              />
+              <div
+                className="header-menu-close"
+                onClick={handleToggleMobileMenu}
+              >
+                <svg viewBox="0 0 1024 1024" className="header-icon02">
+                  <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
+                </svg>
+              </div>
             </div>
           </div>
+
+          {/* Place mobile menu links here */}
+          <nav className="mobile-nav-links">
+            <Link to="/">{props.text}</Link>
+            <Link to="/about">{props.text1}</Link>
+            <Link to="/courses">{props.text2}</Link>
+            <Link to="/blog">{props.text3}</Link>
+            <Link to="/contact">{props.text4}</Link>
+          </nav>
         </div>
-        <div>
-          <svg viewBox="0 0 950.8571428571428 1024" className="header-icon04">
-            <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
-          </svg>
-          <svg viewBox="0 0 877.7142857142857 1024" className="header-icon06">
-            <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
-          </svg>
-          <svg viewBox="0 0 602.2582857142856 1024" className="header-icon08">
-            <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
-          </svg>
-        </div>
-      </div>
+      )}
     </header>
   );
 };
@@ -64,31 +76,27 @@ const Header = (props) => {
 Header.defaultProps = {
   text3: "Blog",
   text4: "Contact",
-  rootClassName: "",
   text1: "About",
   login: "Login",
-  imageSrc1: "https://presentation-website-assets.teleporthq.io/logos/logo.png",
-  imageAlt: "logo",
-  imageAlt1: "image",
-  text2: "Courses",
   register: "Register",
   text: "Home",
   imageSrc: "/design-1500h.webp",
+  imageSrc1: "https://presentation-website-assets.teleporthq.io/logos/logo.png",
+  imageAlt: "Logo",
+  imageAlt1: "Image",
 };
 
 Header.propTypes = {
   text3: PropTypes.string,
   text4: PropTypes.string,
-  rootClassName: PropTypes.string,
   text1: PropTypes.string,
   login: PropTypes.string,
-  imageSrc1: PropTypes.string,
-  imageAlt: PropTypes.string,
-  imageAlt1: PropTypes.string,
-  text2: PropTypes.string,
   register: PropTypes.string,
   text: PropTypes.string,
   imageSrc: PropTypes.string,
+  imageSrc1: PropTypes.string,
+  imageAlt: PropTypes.string,
+  imageAlt1: PropTypes.string,
 };
 
 export default Header;
