a44a814ad49ef32fe8aa48de3c6fca1d79f00c5e
Vijay Krishna Narsingoju <70138450+vknan@users.noreply.github.com>
2024-05-18 16:52:16 +0530

    pythonanyhwere url update

--- navikonline-react/src/services/auth.js
+++ navikonline-react/src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/auth/";
+const API_URL = "https://vknan.pythonanywhere.com/auth/";
 
 const register = (username, email, password) => {
   return axios.post(API_URL + "register/", {
